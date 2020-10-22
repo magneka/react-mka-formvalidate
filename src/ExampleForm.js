0c7055e67e43eb71a1f8e28429b8c551fc4537ad
magneka <magne@alvheim.com>
2020-10-22 12:31:07 +0200

    Begynner på full validering av form

--- src/ExampleForm.js
+++ src/ExampleForm.js
@@ -4,84 +4,93 @@ export function ExampleForm(props) {
 
   //const [name, setName] = useState("");
   
-  const [dirtyState, setDirtyState] = useState({});
+  //const [dirtyState, setDirtyState] = useState({});
+  const [errorMessages, setErrorMessages] = useState({});
   const [formState, setFormState] = useState({
-    //Name: "mka",
-    //Address: "rådal"
+    Name: '',
+    Address: ''
   })
 
-  const isFilled = (val) => {
-    console.log ('validating', val)
-    let result = 'OK'
-    if (new String(val).valueOf() !== new String('MKA').valueOf()) 
-      result = 'Navn ikke mka'
-    console.log ('validating', val, ' res;', result)
+  const isNotBlank= (val, name) => {
+    //console.log ('is not blank', val)
+    if (val) return '' 
+    else return 'Feltet påkrevd. '
+  } 
+  const isFilled = (val, name) => {
+    //console.log ('isFilled', val)
+    let result = ''
+    if (val !== 'MKA')    
+      result = 'Feltet må inneholde "MKA"'
+    //console.log ('validating', val, ' result: ', result)
     return result
   }
+
   const [validatorsState, setValidatorsState] = useState({
     Name: [
-      () => {isFilled(() => formState.Name)}
+      isFilled,
+      isNotBlank
       ]
   });
 
   const handleChange = (e) => {
-    //console.log(e.target.name);
-    //console.log(e.target.value);
-
+    
+    // Get name and input
     let fieldName = e.target.name
     let fieldValue = e.target.value
+
+    // If field has validators run them all
+    if (validatorsState[fieldName]) {
+      let fieldErrors = validatorsState[fieldName].reduce((errors, valFunc) =>{
+        return errors = `${errors}${valFunc(fieldValue, fieldName)} `
+      }, '')
+      
+      setErrorMessages(
+        (prevState => ({...prevState, [fieldName]: fieldErrors})))
+    }
 
     setFormState(
       (prevState => ({...prevState, [fieldName]: fieldValue})))
 
-    setDirtyState(
-      (prevState => ({...prevState, [fieldName]: true})))
+    //setDirtyState(
+    //  (prevState => ({...prevState, [fieldName]: true})))
 
-    let validators = validatorsState[fieldName]
-    validatorsState.Name[0]()
-    console.log (validators)
-    
+  }
 
-    //console.log(JSON.stringify(formState))
-    //console.log(JSON.stringify(dirtyState))
+  const validateAll = () => {
+    Object.keys(formState).forEach(function (key) {
+      var val = formState[key];
+      console.log(val)
+
+      // use val
+    });
+    //formState.keys.map((field, i) => {
+    //  console.log(fieldValue)
+    //})
   }
   
   const handleSubmit = (evt) => {
       evt.preventDefault();
+      validateAll();
       alert(`Submitting Name ${name}`)
   }
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Frirst Name:
-        <input
-          type="text"
-          name="Name"
-          value={formState.Name}
-          onChange={handleChange}
-        />
-      </label>
 
-      <br/>
+      <label>First Name:</label><br/>
+      <input type="text" name="Name" value={formState.Name} onChange={handleChange}/>
+      {errorMessages.Name}<br/>
 
-      <label>
-        Adresss:
-        <input
-          type="text"
-          name="Address"
-          value={formState.Address}
-          onChange={handleChange}
-        />
-      </label>
+      <label>Address:</label><br/>
+      <input type="text" name="Address" value={formState.Address} onChange={handleChange} /><br/>
       
       <br/>
       <input type="submit" value="Submit" />
       <br/>
-      {JSON.stringify(formState)}
-      {JSON.stringify(dirtyState)}
-      {JSON.stringify(validatorsState)}
+      <br/>
+      val:{JSON.stringify(formState)}<br/>      
+      errors: {JSON.stringify(errorMessages)}<br/>      
       <br />
-      //val:{validatorsState.Name[0]()}
+      
 
     </form>
   );
